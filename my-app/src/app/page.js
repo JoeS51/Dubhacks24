0e246501336f4af7314a28136233d27f7d502941
Johannes Sluis <joesluis51@gmail.com>
2024-10-12 14:59:50 -0700

    map yuhyuh

--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ParkingGrid from "./Components/ParkingGrid";
+<<<<<<< HEAD
 import Layout from './Components/Layout';
+=======
+import Map from './Components/Map';
+>>>>>>> 9947f23 (map yuhyuh)
 
 export default function Home() {
   return (
