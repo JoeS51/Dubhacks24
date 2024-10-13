34d0a9e378abd5546a64d2959b1302383487506b
Johannes Sluis <joesluis51@gmail.com>
2024-10-13 04:42:26 -0700

    parking cards show from db

--- my-app/src/app/Components/ParkingGrid.js
+++ my-app/src/app/Components/ParkingGrid.js
@@ -1,13 +1,60 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ParkingCard from './ParkingCard';
 import parkingSpot1 from '../Images/parkingspot.jpg'
 import '../Styles/ParkingGrid.css'
+import axios from 'axios';
+import Park from '../../../public/Park';
 
 const ParkingGrid = () => {
+
+  const formatDate = (dateStr) => {
+    const date = new Date(dateStr);
+    return date.toLocaleString('en-US', {
+      weekday: 'short', // E.g., Sun
+      month: 'short',   // E.g., Dec
+      day: 'numeric',   // E.g., 15
+      hour: 'numeric',  // E.g., 10 AM
+      minute: '2-digit',
+      hour12: true,     // Use AM/PM format
+      timeZone: 'UTC',  // Adjust as needed
+    });
+  };
+
+  const [listings, setListings] = useState([]);
+
+  useEffect(() => {
+    // Function to fetch data
+    const fetchListings = async () => {
+      try {
+        const response = await axios.get('http://localhost:4000/api/listings/get');
+        console.log('API Response:', response.data); // Log the JSON data to console
+        setListings(response.data); // Optionally, store data in state
+      } catch (error) {
+        console.error('Error fetching listings:', error);
+      }
+    };
+
+    fetchListings();
+  }, []); // Empty dependency array to run once on mount
+
   return (
     <div className="container">
       <div className="grid-container">
-        <ParkingCard
+        {listings.map((listing) => (
+
+          <ParkingCard
+            image={parkingSpot1}
+            title={listing.title}
+            location={listing.address.city + ", " + listing.address.state}
+            rating={listing.rating}
+            reviews={listing.numRatings}
+            priceNow={listing.price}
+            dates={`Now - ${formatDate(listing.end)}`}
+            key={listing._id}
+          />
+
+        ))}
+        {/* <ParkingCard
           image={parkingSpot1}
           title="Guest suite in Seattle"
           location="Seattle, WA"
@@ -74,7 +121,7 @@ const ParkingGrid = () => {
           priceBefore={116}
           priceNow={68}
           dates="Oct 12 - 17"
-        />
+        /> */}
       </div>
     </div>
   );
