c43e27ca83fdf3f7b7db1a27ec42d7e711db7a25
Sheepca <mtrien01@yahoo.com>
2024-10-13 03:47:19 -0700

    awesome

---
+++ backend/model/listing.model.js
@@ -0,0 +1,60 @@
+import mongoose from 'mongoose';
+
+const listingSchema = new mongoose.Schema({
+  parentUsername: {
+    type: String,
+    required: true
+  },
+  position: {
+    type: {
+      type: String, 
+      enum: ['Point'], 
+      required: true
+    },
+    coordinates: {
+      type: [Number], // Array of numbers representing longitude and latitude
+      required: true
+    }
+  },
+  address: {
+    city: {
+      type: String,
+      required: true
+    },
+    state: {
+      type: String,
+      required: true
+    },
+    zipcode: {
+      type: String,
+      required: true
+    },
+    street: {
+      type: String,
+      required: true
+    }
+  },
+  start: {
+    type: Date, // Use Date type for timestamps
+    required: true
+  },
+  end: {
+    type: Date, // Use Date type for timestamps
+    required: true
+  },
+  price: {
+    type: Number, // Use Number type for price
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  }
+}, { _id: true }); // MongoDB generates _id automatically, but you can define it explicitly
+
+// Ensure the position field is indexed for geospatial queries
+listingSchema.index({ position: '2dsphere' });
+
+const Listing = mongoose.model('Listing', listingSchema);
+
+export default Listing;
