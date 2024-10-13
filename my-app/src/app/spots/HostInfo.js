acda1c53efc232e081bc009fbd436d4290059d4c
heonjwa <heonjwa@gmail.com>
2024-10-12 22:52:41 -0700

    organized screen

--- my-app/src/app/spots/HostInfo.js
+++ my-app/src/app/spots/HostInfo.js
@@ -1,12 +1,14 @@
+import Image from 'next/image';
 import React from 'react';
+import trien from '../Images/trien_vuong.jpg'
 
 // components/HostInfo.js
 const HostInfo = () => {
     return (
       <div className="flex items-center mb-8">
-        <img src="/api/placeholder/50/50" alt="Host" className="rounded-full mr-4" />
+        <Image src={trien} width="60" height="60" className='rounded-full mr-4'/>
         <div>
-          <h3 className="font-semibold">Stay with Rosemary</h3>
+          <h3 className="font-semibold">Minh Trenithing Vuong</h3>
           <p className="text-gray-600">2 months hosting</p>
         </div>
       </div>
--- my-app/src/app/spots/ListingInfo.js
+++ my-app/src/app/spots/ListingInfo.js
@@ -4,8 +4,8 @@ import React from 'react';
 const ListingInfo = () => {
     return (
       <div className="mb-8">
-        <h2 className="text-xl font-semibold mb-2">Room in a home hosted by Rosemary</h2>
-        <p>1 bed · Shared bathroom</p>
+        <h2 className="text-xl font-semibold mb-2">Parking hosted by Trien</h2>
+        <p>Gated · SUV, Sedan, Van</p>
         <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">New</span>
       </div>
     );
