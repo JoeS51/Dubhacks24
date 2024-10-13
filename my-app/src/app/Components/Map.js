43e6556f876be9f091a126c4b7e9b848943e9782
Johannes Sluis <joesluis51@gmail.com>
2024-10-12 21:49:15 -0700

    done

--- my-app/src/app/Components/Map.js
+++ my-app/src/app/Components/Map.js
@@ -1,26 +1,42 @@
 "use client";
 
-import React from 'react'
+import React, { useState } from 'react';
 import {
   APIProvider,
   Map,
   AdvancedMarker,
   Pin,
   InfoWindow
-} from "@vis.gl/react-google-maps"
+} from "@vis.gl/react-google-maps";
 
 export default function GoogleMap() {
-  const pos = {lat: 47.65, lng: -122.3};
+  const initialPos = {lat: 47.65, lng: -122.3};
+  const [center, setCenter] = useState(initialPos);
+
+  const onMapDrag = (e) => {
+    const newCenter = e.detail.center;
+    setCenter(newCenter);
+  };
 
   return (
     <APIProvider apiKey='AIzaSyCkuzW1WbWBbDGhQsT5aIKR2q4ww1RnFKQ'>
-      <div style={{height: "100vh", width: "100vh"}}>
-        <Map zoom={15} center={pos} mapId='f743d2c8c09f3efe'>
-          <AdvancedMarker position={pos}>
-            
+      <div style={{height: "100vh", width: "100%"}}>
+        <Map
+          defaultCenter={initialPos}
+          center={center}
+          mapId='f743d2c8c09f3efe'
+          scrollwheel={true}
+          zoomControl={true}
+          minZoom={14}
+          maxZoom={20} 
+          disableDefaultUI={true}
+          onDrag={onMapDrag}
+        >
+          <AdvancedMarker position={initialPos}>
+            <Pin />
           </AdvancedMarker>
         </Map>
       </div>
     </APIProvider>
-  )
+  );
 }
